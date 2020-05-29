fcd1f92ed3b3cb70f6707fb6f96d3d91f649c670
hsgonzalmu <hsgonzalmu@gmail.com>
2020-05-29 22:33:40 +0000

    practicando con dplyr

--- R/primer_script.R
+++ R/primer_script.R
@@ -1,3 +1,8 @@
+
+
+# Cosas Varias ------------------------------------------------------------
+
+
 # Este es un comentario
 # Cargar librerias
 library(tidyverse)
@@ -122,6 +127,138 @@ vuelos %>%
   geom_col()
 
 
+
+
+
+
+
+
+# Cosas de Dplyr ----------------------------------------------------------
+
+# Cuando tenemos una tabla, en general se aplican los iguientes verbos:
+
+# Filtar
+
+tabla1 %>% 
+  filter(pais == "China")
+
+tabla1 %>% 
+  filter(pais %in% c("Brasil", "China"))
+
+tabla1 %>% 
+  filter(!pais == "Afganistán")
+
+tabla1 %>% 
+  filter(pais != "Afganistán")
+
+tabla1 %>% 
+  filter(str_detect(pais, "Ch"))
+
+tabla1 %>% 
+  select(starts_with("a"))
+
+# Crear variables nuevas
+
+tabla1 %>% 
+  mutate(porcentaje_de_tuberculosis = casos/poblacion)
+# Libreia para dar formato a las variables
+library(scales)
+
+
+tabla1 %>% 
+  mutate(porcentaje_de_tuberculosis = casos/poblacion) %>% 
+  mutate(porcentaje_de_tuberculosis = percent(porcentaje_de_tuberculosis))
+
+tabla1 %>% 
+  mutate(porcentaje_de_tuberculosis = casos/poblacion) %>% 
+  mutate(porcentaje_de_tuberculosis = round(casos/poblacion*100, 2)) %>% 
+  mutate(porcentaje_de_tuberculosis = percent(porcentaje_de_tuberculosis/100))
+
+tabla1 %>% 
+  mutate(
+    porcentaje_de_tuberculosis = percent(round(casos/poblacion*100,2)/100)
+  )
+
+tabla1 %>% 
+  mutate(
+    porcentaje_de_tuberculosis = percent(
+      round(
+        casos/poblacion*100,2)/100
+      )
+  )
+  
+tabla1 %>% 
+  mutate(
+    pais = tolower(pais),
+    anio = as.character(anio),
+    casos = casos/1e6
+  )
+
+# select
+
+tabla1 %>% 
+  select(anio, pais)
+
+tabla1 %>% 
+  select(2,1)
+
+tabla1 %>% 
+  select(1:3)
+
+tabla1 %>% 
+  select(1,2,3)
+
+# Resumir 
+
+
+tabla1 %>% 
+  summarise(casos = sum(casos))
+
+
+tabla1 %>% 
+  group_by(pais) %>% 
+  summarise(casos = sum(casos))
+
+tabla1 %>% 
+  group_by(pais) %>% 
+  summarise(casos = sum(casos),
+            poblacion_promedio = mean(poblacion))
+
+tabla1 %>% 
+  group_by(pais) %>% 
+  summarise(casos = sum(casos),
+            poblacion_promedio = mean(poblacion)) %>% 
+  ungroup() %>% 
+  mutate(infectados_promedio = casos / poblacion_promedio)
+
+# Juntar
+
+tabla4a
+tabla4b
+
+
+tabla4a %>% 
+  left_join(tabla4b, by = "pais")
+
+tabla4a %>% 
+  left_join(tabla4b, by = "pais", suffix = c("_casos", "_poblacion"))
+
+
+
+tabla4b %>% 
+  left_join(tabla4a, by = "pais", suffix = c( "_poblacion", "_casos"))
+
+# spread gather
+
+tabla4a
+
+tabla1 %>% 
+  select(1:3) %>% 
+  spread(anio, casos)
+
+
+tabla4a %>% 
+  gather(anio, casos, -pais)
 
 
 
